8a1ba25f0a60def7c4f0f469f3529a1600d11a72
Polo280 <jorglezd28@gmail.com>
2024-05-14 19:40:26 -0600

    Fixed delay issues

--- Custom Libraries/Systick Config/Clocks.h
+++ Custom Libraries/Systick Config/Clocks.h
@@ -6,21 +6,33 @@
 #include "main.h"
 
 //////////// MACROS ////////////
-#define CLK_MILLIS          (SystemCoreClock/1000) - 1
-#define CLK_HUNDRED_MICROS  (SystemCoreClock/10000) - 1
-#define CLK_TENS_MICROS     (SystemCoreClock/100000) - 1
-#define CLK_MICROS          (SystemCoreClock/1000000) - 1
+
+// Conversions
+#define MILLIS2HUNDRED_MICROS  10
+#define MILLIS2TENS_MICROS     100
+#define MILLIS2MICROS          1000
 
 /////////// Variables //////////
+extern uint16_t conv_factor;
+
+enum TIME_UNIT{
+	CLK_MILLIS          = 0,
+	CLK_HUNDRED_MICROS  = 1,
+	CLK_TENS_MICROS     = 2,
+	CLK_MICROS          = 3
+};
 
 /////////// Functions //////////
-void ClkInit(uint32_t);
-void ClkInitIt(uint32_t);
+void MCG_Config(void);
+void ClkInit(uint8_t);
+void ClkInitIt(uint8_t);
 void ClkFreqConfig(uint32_t);
+uint32_t MapFrequency(uint8_t);
 
 // Getters & setters
-uint64_t GetSysTicks(void);
+uint64_t* GetSysTicks(void);
 void SetDelayTicks(uint32_t);
+void SetConvFactor(uint8_t);
 
 // Delay Functions
 void delayMs(uint32_t);
